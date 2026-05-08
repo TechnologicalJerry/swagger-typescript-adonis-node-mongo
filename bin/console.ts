3ce6d54ed636004a272875eeb7d93f98f404d8d1
Technologicaljerry <cserajatverma@gmail.com>
2026-05-08 23:59:36 +0530

    feat: initialize AdonisJS project structure with MongoDB provider and base configurations

---
+++ bin/console.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata'
+import { Ignitor } from '@adonisjs/core/env'
+
+const APP_ROOT = new URL('../', import.meta.url)
+const IMPORTER = (filePath: string) => {
+  if (filePath.startsWith('./') || filePath.startsWith('../')) {
+    return import(new URL(filePath, APP_ROOT).href)
+  }
+  return import(filePath)
+}
+
+new Ignitor(APP_ROOT, { importer: IMPORTER })
+  .tap((app) => {
+    app.booting(async () => {
+      await import('#start/env')
+    })
+  })
+  .ace()
+  .handle(process.argv.splice(2))
