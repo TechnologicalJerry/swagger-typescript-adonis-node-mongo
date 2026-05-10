6249660120c5bfede10984ef38d7e84360d1dd4b
Technologicaljerry <cserajatverma@gmail.com>
2026-05-10 17:28:09 +0530

    feat: initialize core server, routing, Swagger documentation, and HTTP context types

---
+++ start/kernel.ts
@@ -0,0 +1,33 @@
+import router from '@adonisjs/core/services/router'
+import server from '@adonisjs/core/services/server'
+
+/**
+ * The error handler is used to convert an exception
+ * to a HTTP response.
+ */
+server.errorHandler(() => import('#app/exceptions/handler'))
+
+/**
+ * The server middleware stack runs middleware on all the HTTP
+ * requests, even if there is no route registered for
+ * the request URL.
+ */
+server.use([
+  () => import('#middleware/force_json_response_middleware'),
+  () => import('@adonisjs/cors/cors_middleware')
+])
+
+/**
+ * The router middleware stack runs middleware on all the HTTP
+ * requests with a registered route.
+ */
+router.use([() => import('@adonisjs/core/bodyparser_middleware')])
+
+/**
+ * Named middleware collection must be explicitly assigned to
+ * the routes or the routes group.
+ */
+export const middleware = router.named({
+  auth: () => import('#middleware/auth_middleware'),
+  role: () => import('#middleware/role_middleware')
+})
